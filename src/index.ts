e9778d1e721c86cf755aa1113bde7a4cf82bc3ed
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-03-28 12:05:11 +0530

    Removed & Added : Found the native & Tokens transfer transactions

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,194 @@
-import { ethers, providers } from 'ethers';
-import { Request, Response } from 'express';
-import { insertBlockDetails } from './blockProcessor';
+type Transaction = {
+    hash: string;
+    blockHash: string;
+    block_number: number;
+    timestamp: number;
+    inputs?: {
+        outputRef?: {
+            hint?: number,
+            key?: string,
+        },
+        unlockScript?: string,
+        txHashRef?: string,
+        address?: string,
+        attoAlphAmount?: string,
+        tokens: {
+            id?: string,
+            amount?: string,
+        }[],
+    }[];
+    outputs?: {
+        type: string;
+        hint: number;
+        key: string;
+        attoAlphAmount: string;
+        address: string;
+        tokens:{
+            id:string;
+            amount:string;
+        }[];
+        lockTime: number;
+        message: string;
+    }[];
+    gasAmount: number;
+    gasPrice: string;
+    scriptExecutionOk: boolean;
+    coinbase: boolean;
+};
 
-const express = require("express");
+type WalletExplorerTransaction = {
+    hash: string;
+    block_number: number;
+    wallet: string;
+    contract: string;
+    timestamp: number;
+    from: string;
+    to: string;
+    tx_cost?: string | null;
+    amount: string;
+    value_usd: number;
+    method_id: string;
+    is_out?: boolean;
+    is_transfer?: boolean;
+    fee?: number;
+    is_added?: boolean;
+    is_removed?: boolean;
 
+    //from db:
+    id?: number;
+    portfolio_id?: number;
+}
 
-
-const blockRoutes = require("./blocks/route");
-const transactionRoutes = require("./transactions/route");
-const logsRoutes = require("./logs/route");
-
-const app = express();
-const PORT = 3000;
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/api/v1/blocks', blockRoutes);
-app.use('/api/v1/txn', transactionRoutes);
-app.use('/api/v1/logs', logsRoutes);
-
-// Root route
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello sacha! sup!');
-});
-
-
-// Error handling middleware
-app.use((err: Error, req: Request, res: Response, next: Function) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
-
-
-function initializeProvider(): ethers.providers.Provider {
-    // const apikey = "Yn0SxinPuP7HJoNuprd6sq1hOJ_7BvZ8"; // Alchemy API KEY
-    const apikey = "McqRqcKVY-8JkRwFPxpw0gkcly_d862w";
-    const alchemyProvider = new ethers.providers.AlchemyProvider("mainnet", apikey);
-    const ankrRpcProvider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/eth");
-
-    // Create FallbackProviderConfig for each backend provider
-    const alchemyConfig = {
-        provider: alchemyProvider,
-        priority: 1, // Example priority
-        weight: 1, // Example weight
-        stallTimeout: 5000 // Example stall timeout in milliseconds
-    };
-
-    const ankrRpcConfig = {
-        provider: ankrRpcProvider,
-        priority: 2,
-        weight: 1,
-        stallTimeout: 3000
-    };
-
-    // Add more FallbackProviderConfig objects for additional backend providers if needed
-
-    // Create FallbackProvider with the configured backend providers
-    const fallbackProvider = new ethers.providers.FallbackProvider([alchemyConfig, ankrRpcConfig]); // Add more configs if needed
-
-    // Log provider initialization
-    console.log('Ethers.js FallbackProvider initialized');
-
-    // Return the initialized FallbackProvider
-    return fallbackProvider;
+type BlockHash = {
+    hash: string,
+    timestamp: number,
+    chainFrom: number,
+    chainTo: number,
+    height: number,
+    txNumber: number,
+    mainChain: true,
+    hashRate: string,
 }
 
 
-
-
-
-async function listenForBlocks(provider: ethers.providers.Provider) {
-
-    //Event listener for new blocks
-    provider.on("block", async (blockNumber: number) => {
-        try {
-            // Insert block details into the database
-            await insertBlockDetails(provider, blockNumber);
-            
-        } catch (error) {
-            console.error("Error handling block event:", error);
-        }  
-    });
+async function getBlockNumber(blockHash:string) {
+    try {
+        const blockEndpoint = `https://backend.mainnet.alephium.org/blocks/${blockHash}`;
+        const response = await fetch(blockEndpoint);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch block number for block hash ${blockHash}`);
+        }
+        const data = await response.json();
+        return data.height;
+    } catch (error) {
+        console.error(`Error fetching block number for block hash ${blockHash}:`, error);
+        throw error;
+    }
 }
-      
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
 
-// Main function to initialize provider and start listening for blocks
+async function getTransactionsForAddress(address: string, page: number, limit: number): Promise<WalletExplorerTransaction[]> {
+    try {
+        const walletTxnPoint: string = `https://backend.mainnet.alephium.org/addresses/${address}/transactions?page=${page}&limit=${limit}`;
+        const response = await fetch(walletTxnPoint);
+        if (!response.ok) {
+            if (response.status === 400) {
+                throw new Error('Bad request: Something bad in the request');
+            } else if (response.status === 401) {
+                throw new Error('Unauthorized');
+            } else if (response.status === 404) {
+                throw new Error('Not found');
+            } else {
+                throw new Error('Failed to fetch transactions');
+            }
+        }
+        const data: Transaction[] = await response.json();
+        const formatTxnPromises = data.map((txn: Transaction) => formatTransaction(txn, address));
+        const formatTxn = await Promise.all(formatTxnPromises);
+        return formatTxn;
+    } catch (error) {
+        console.error("Error on Fetching Wallet Transactions:", error);
+        throw error;
+    }
+}
+
+
+async function formatTransaction(data: Transaction, address: string): Promise<WalletExplorerTransaction> {
+   const tx_cost = (Number(data.gasPrice) * data.gasAmount) / (10 ** 18);
+   const isOut = data.inputs?.some(input => input.address === address);
+   const contract = data.inputs?.flatMap(input => input.tokens?.map(token => token.id)).filter(Boolean).join(', ') || "";
+
+   const from = Array.isArray(data.inputs) 
+   ? [...new Set(data.inputs.map(input => input.address))].join(', ') 
+   : '';
+ 
+   const inputHint = data.inputs?.[0]?.outputRef?.hint;
+   const receiver = data.outputs?.find(output => output.hint !== inputHint);
+   const to = receiver?.address || '';
+    let amountReceived;
+    if(receiver?.tokens) {
+      const amount = receiver.tokens.find(token => token.id === data.inputs?.[0]?.tokens?.[0]?.id);
+      amountReceived = amount?.amount || 0;   
+    }else{
+        amountReceived = receiver?.attoAlphAmount || 0;
+    }
+
+   const formattedTransaction: WalletExplorerTransaction = {
+      hash: data.hash,
+      block_number: await getBlockNumber(data.blockHash), // Populate as needed
+      wallet: address, // Populate as needed
+      contract: contract,
+      timestamp: data.timestamp,
+      from: from,
+      to: to,
+      tx_cost: tx_cost.toString(),
+      amount: String(amountReceived),
+      value_usd: 0,
+      method_id: "",
+      is_out: isOut,
+      is_transfer: false,
+      fee: data.gasAmount,
+      is_added: false,
+      is_removed: false,
+   };
+
+   return formattedTransaction;
+}
+
+
 async function main() {
     try {
-
-        const provider = initializeProvider(); // Initialize Ethereum provider
-        await listenForBlocks(provider); // Listen for new blocks
-
+        const result = await getTransactionsForAddress("1Bt4D1D1RMqtZ4JFrpUKoUe5rZkvs1MZ7hnepQA6dn9U4", 1, 6);
+        console.log(JSON.stringify(result, null, 2));
     } catch (error) {
         console.error('Error starting server:', error);
         process.exit(1);
     }
 }
 
-// Execute main function
 main().catch(error => {
     console.error('Error:', error);
     process.exit(1);
 });
+
+
+
+
+// function analyzeTransaction(transaction: Transaction) {
+//     // Check if the transaction involves token transfer
+//     if (transaction.inputs.length > 0 && transaction.outputs.length > 0) {
+//         const inputFromYourAddress = transaction.inputs.find(input => input.address === '1Bt4D1D1RMqtZ4JFrpUKoUe5rZkvs1MZ7hnepQA6dn9U4');
+//         const outputToYourAddress = transaction.outputs.find(output => output.address === '1Bt4D1D1RMqtZ4JFrpUKoUe5rZkvs1MZ7hnepQA6dn9U4');
+        
+//         if (inputFromYourAddress && outputToYourAddress) {
+//             // Transaction pattern: Your address -> Contract/Swap -> Your address
+//             console.log('Transaction pattern: Your address -> Contract/Swap -> Your address');
+//             const tokensSentToContract = inputFromYourAddress.tokens?.find(token => token.id !== '1a281053ba8601a658368594da034c2e99a0fb951b86498d05e76aedfe666800');
+//             const tokensReceivedBack = outputToYourAddress.tokens?.find(token => token.id === '1a281053ba8601a658368594da034c2e99a0fb951b86498d05e76aedfe666800');
+//             if (tokensSentToContract && tokensReceivedBack) {
+//                 console.log(`Amount sent to contract: ${tokensSentToContract.amount}`);
+//                 console.log(`Amount received back: ${tokensReceivedBack.amount}`);
+//             }
+//         }
+//     }
+// }
+
