a20cbb93035a602163b0cf41f8a0d2e63c2a2f13
sanjayshanmugam sk <sanjayshanmugamsk2108@gmail.com>
2024-04-24 23:09:48 +0530

    Updated the limit

--- src/transactions/route.ts
+++ src/transactions/route.ts
@@ -1,27 +1,31 @@
-import { Router, Request, Response } from "express";
-const controller = require('./controller');
+import { Request, Response, Router } from "express";
+const controller = require("./controller");
 
 const router: Router = Router();
 
 // Adjusted to use query parameters for page and limit
 router.get("/:address", async (req: Request, res: Response) => {
-    try {
-        // Extracting `address` from the route parameters
-        const { address } = req.params;
-        // `page` and `limit` are now optional query parameters
-        // Use a default value if they're not provided or if parsing fails
-        const page = parseInt(req.query.page as string) || 1;
-        const limit = parseInt(req.query.limit as string) || 100;
+  try {
+    // Extracting `address` from the route parameters
+    const { address } = req.params;
+    // `page` and `limit` are now optional query parameters
+    // Use a default value if they're not provided or if parsing fails
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 5;
 
-        // Assuming `getTransactionsForAddress` is adjusted to take these parameters directly
-        const transactions = await controller.getTransactionsForAddress(address, page, limit);
-        
-        // Assuming `getTransactionsForAddress` returns the transactions
-        res.json(transactions);
-    } catch (error) {
-        console.error("Error processing request:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+    // Assuming `getTransactionsForAddress` is adjusted to take these parameters directly
+    const transactions = await controller.getTransactionsForAddress(
+      address,
+      page,
+      limit
+    );
+
+    // Assuming `getTransactionsForAddress` returns the transactions
+    res.json(transactions);
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
